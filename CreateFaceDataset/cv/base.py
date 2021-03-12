bfea06615fb64b4584bfb6b343764c185e69927e
scutzhe <scuteezxz@163.com>
2021-03-12 09:45:25 +0800

    ooo

--- CreateFaceDataset/cv/base.py
+++ CreateFaceDataset/cv/base.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
-# @author  : 郑祥忠
-# @license : (C) Copyright,2016-2020,广州海格星航科技
-# @contact : dylenzheng@gmail.com
-# @file    : base.py
-# @time    : 8/14/20 2:54 PM
-# @desc    : 
-'''
+
 
 import numpy as np
 import cv2
